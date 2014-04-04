2ff7b5cd72a402df2746b67774881f02fbc00456
Madhusudan Banik <madhusudan.banik@edenrockcomm.com>
2014-04-03 18:47:17 -0700

    copy

--- decorator_thread.py
+++ decorator_thread.py
@@ -2,20 +2,22 @@ __author__ = 'msbanik'
 # Thread implementation with decorator
 import threading
 
+
 class thread:
-	def __init__(self, f):
-		self.f = f
-	def __call__(self, *args, **kwargs):
-		t = threading.Thread(target=self.f, args=args)
-		t.setDaemon(True)
-		t.start()
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, *args, **kwargs):
+        t = threading.Thread(target=self.f, args=args)
+        t.setDaemon(True)
+        t.start()
 
 
 @thread
 def sum(x, y):
-	print threading.currentThread().getName()
-	print 'Sum of {0} and {1} is: {2} \n'.format(x, y, x + y)
+    print threading.currentThread().getName()
+    print 'Sum of {0} and {1} is: {2} \n'.format(x, y, x + y)
 
 # running each sum operation on different thread
-sum(2, 3)	
+sum(2, 3)
 sum(4, 5)	
--- decorator_with_arg_class.py
+++ decorator_with_arg_class.py
@@ -1,15 +1,22 @@
 class logger:
-	def __init__(self, *args, **kwargs):
-		self.args = args
-		self.kwargs = kwargs
-	def __call__(self, func):
-		print self.args, self.kwargs
-		def outer(*args, **kwargs):
-			func(*args, **kwargs)
-		return outer
+    def __init__(self, *args, **kwargs):
+        print args
+        # ('DEBUG', 'TriBug')
+        self.args = args
+        self.kwargs = kwargs
 
-@logger('DEBUG')
+    def __call__(self, func):
+        def outer(*args, **kwargs):
+            print args
+            # (2, 3)
+            func(*args, **kwargs)
+
+        return outer
+
+
+@logger('DEBUG', 'TriBug')
 def sum(x, y):
-	print 'Sum of {0} and {1} is: {2}'.format(x, y, x + y)
+    print 'Sum of {0} and {1} is: {2}'.format(x, y, x + y)
 
-sum(2, 3)	
+
+sum(2, 3)
